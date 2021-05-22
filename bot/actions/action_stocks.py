1cbaf2edb48c12acd755dbbdb4aa49c2f7e8d167
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2021-05-21 22:15:08 -0300

    stocks actions example

--- bot/actions/action_stocks.py
+++ bot/actions/action_stocks.py
@@ -4,23 +4,25 @@
 # Veja o guia na documentação do RASA em:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-import requests
+import http.client
 
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
-
-querystring = {"q":"tesla","region":"BR"}
+conn = http.client.HTTPSConnection("apidojo-yahoo-finance-v1.p.rapidapi.com")
 
 headers = {
     'x-rapidapi-key': "00265dacebmsh5806171a71a7de1p1c9fa8jsnf5940c6fd9fc",
     'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+conn.request("GET", "/auto-complete?q=tesla&region=US", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
 
 class ActionStock(Action):
     def name(self) -> Text:
@@ -33,7 +35,7 @@ class ActionStock(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
         try:
-            dispatcher.utter_message(print(response.text))
+            dispatcher.utter_message(data)
         except ValueError:
-            dispatcher.utter_message(ValueError)
+             dispatcher.utter_message(ValueError)
         return []
